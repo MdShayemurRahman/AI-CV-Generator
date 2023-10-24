fd19501ac5dc8927bc45ad31b30b479a6d947705
Md Shayemur Rahman <mdshayemur.rahman@tuni.fi>
2023-10-24 18:39:19 +0300

    file structure for frontend and backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import './App.css';
 import Homepage from './pages/Homepage';
 import Layout from './Layout';
 import PageNotFound from './pages/PageNotFound';
