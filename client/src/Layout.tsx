21a2ca59912fda91aae5903e2a0c7883a9faf4ac
Md Shayemur Rahman <mdshayemur.rahman@tuni.fi>
2023-10-26 00:38:19 +0300

    navbar design

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -1,22 +1,29 @@
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+
+const title = {
+  flexGrow: 1,
+};
 
 const Layout = () => {
+  const navigate = useNavigate();
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/blogs'>Blogs</Link>
-          </li>
-          <li>
-            <Link to='/contact'>Contact</Link>
-          </li>
-        </ul>
-      </nav>
-
+      <AppBar position='static'>
+        <Toolbar sx={{ margin: '2rem 5rem'}}>
+          <Typography variant='h4' sx={title}>
+            Ai-CV Generator
+          </Typography>
+          <Box>
+            <Button color='inherit' onClick={() => navigate('/')}>
+              Home
+            </Button>
+            <Button color='inherit' onClick={() => navigate('/login')}>
+              Login
+            </Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
       <Outlet />
     </>
   );
