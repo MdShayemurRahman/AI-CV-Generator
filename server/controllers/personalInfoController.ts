76d8dd5a0736779b413b073b05d5373fa9f20ded
Nikke Karaksela <nikke.karaksela@tuni.fi>
2023-11-03 09:37:28 +0200

    Updated personalInfoController and personalInfoRouter

--- server/controllers/personalInfoController.ts
+++ server/controllers/personalInfoController.ts
@@ -39,8 +39,7 @@ export const getPersonalInfoById = (req: Request, res: Response) => {
   }
 };
 
-// Method 3: Create personal info
-
+// Method 3: Add personal info
 export const addPersonalInfo = (req: Request, res: Response) => {
   try {
     const newPersonalInfo: PersonalInfo = req.body;
@@ -78,6 +77,56 @@ export const addPersonalInfo = (req: Request, res: Response) => {
   }
 };
 
-// 4. Edit
+// Method 4: Update personal info
+export const updatePersonalInfo = (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const updatedAttributes = req.body;
+
+    // Find the personal information by ID
+    const personalInfoToUpdate = personalInfo.find(
+      (info) => info.id === Number(id)
+    );
+
+    if (!personalInfoToUpdate) {
+      return res
+        .status(404)
+        .json({ message: "Personal information not found" });
+    }
+
+    // Update the specified attributes with the new values
+    Object.keys(updatedAttributes).forEach((attribute) => {
+      personalInfoToUpdate[attribute] = updatedAttributes[attribute];
+    });
+
+    return res.json({ message: "Personal information updated successfully" });
+  } catch (error) {
+    console.error("Error updating personal information:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 // 5.Delete
+
+export const deletePersonalInfo = (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    // Find the index of the personal information by ID
+    const index = personalInfo.findIndex((info) => info.id === Number(id));
+
+    if (index === -1) {
+      return res
+        .status(404)
+        .json({ message: "Personal information not found" });
+    }
+
+    // Remove the personal information entry from the array
+    personalInfo.splice(index, 1);
+
+    return res.json({ message: "Personal information deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting personal information:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
--- server/routers/personalInfoRouter.ts
+++ server/routers/personalInfoRouter.ts
@@ -3,6 +3,8 @@ import {
   getAllPersonalInfo,
   getPersonalInfoById,
   addPersonalInfo,
+  updatePersonalInfo,
+  deletePersonalInfo,
 } from "../controllers/personalInfoController";
 
 const personalInfoRouter: Router = express.Router();
@@ -10,5 +12,7 @@ const personalInfoRouter: Router = express.Router();
 personalInfoRouter.get("/api/resume/personal", getAllPersonalInfo);
 personalInfoRouter.get("/api/resume/personal/:id", getPersonalInfoById);
 personalInfoRouter.post("/api/resume/personal/", addPersonalInfo);
+personalInfoRouter.put("/api/resume/personal/:id", updatePersonalInfo);
+personalInfoRouter.delete("/api/resume/personal/:id", deletePersonalInfo);
 
 export default personalInfoRouter;
