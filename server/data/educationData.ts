c0d497f74c931061fe468cc4d6494db657e68626
Mohamed EL Fechtali <fechtali@Mohameds-MacBook-Pro.local>
2023-11-08 15:56:57 +0200

    Education Controllers

---
+++ server/data/educationData.ts
@@ -0,0 +1,20 @@
+import { Education } from '../types/Education';
+
+export const educationData: Education[] = [
+    {
+      id: 1,
+      institute: "University of XYZ",
+      degree: "Bachelor in Software Engineering",
+      graduation_date: "May 2023",
+      gpa: "3.8",
+      field_of_study: "Computer Science",
+    },
+    {
+      id:2,
+      institute: "University of XYZ",
+      degree: "Bachelor in Software Engineering",
+      graduation_date: "May 2023",
+      gpa: "3.8",
+      field_of_study: "Computer Science",
+    },
+  ];
--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import personalInfoRouter from "./routers/personalInfoRouter";
 import express, { Express, Request, Response } from 'express';
 import skillsRouter from './routers/skillsRouter';
+import educationRouter from './routers/educationRouter';
 import { loggingMiddleware } from './middlewares/logging';
 import { apiErrorHandler } from './middlewares/apiErrorHandler';
 import { routeNotFound } from './middlewares/routeNotFound';
@@ -15,6 +16,8 @@ app.use(express.json());
 app.use("/", personalInfoRouter);
 
 app.use('/api/resume/skills', loggingMiddleware, skillsRouter);
+app.use('/api/resume/education', loggingMiddleware, educationRouter);
+
 
 app.use(apiErrorHandler);
 //app.use(routeNotFound); //
