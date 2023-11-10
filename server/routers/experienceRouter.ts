2b57ca8b871ea901eda7abc835f16e178168bbb1
alex <alexander.anchor@tuni.fi>
2023-11-10 12:37:53 +0200

    experience data

---
+++ server/routers/experienceRouter.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import {
+    getAllExperience,
+    getExperienceById,
+    createExperience,
+    updateExperience,
+    deleteExperience,
+} from "../controllers/experienceDataController";
+
+const experienceRouter = Router();
+
+experienceRouter.get('/', getAllExperience);
+experienceRouter.get('/:id', getExperienceById);
+experienceRouter.post('/', createExperience);
+experienceRouter.patch('/:id', updateExperience);
+experienceRouter.delete('/:id', deleteExperience);
+
+export default experienceRouter;
