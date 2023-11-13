eec0f2c8267c901f8510c826d98322bcb04af5a1
Nikke Karaksela <nikke.karaksela@tuni.fi>
2023-11-13 12:11:32 +0200

    Update personalInfoSchema

--- server/schemas/personalInfoSchema.ts
+++ server/schemas/personalInfoSchema.ts
@@ -1,6 +1,19 @@
 import { z } from "zod";
 
-export const PersonalInfoBodySchema = z.object({});
+export const PersonalInfoBodySchema = z.object({
+  first_name: z.string(),
+  last_name: z.string(),
+  email: z.string(),
+  phone_no: z.string(),
+  linkedin_profile: z.string(),
+  personal_website: z.string(),
+  address: z.object({
+    street_addr: z.string(),
+    city: z.string(),
+    country: z.string(),
+  }),
+  headline: z.string(),
+});
 
 export const PersonalInfoSchema = z.object({
   body: PersonalInfoBodySchema,
