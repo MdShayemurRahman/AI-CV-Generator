b90c593e23cb6aaf2fdeaeb93670738063311b2e
Md Shayemur Rahman <mdshayemur.rahman@tuni.fi>
2023-11-21 22:14:01 +0200

    Project Schema updated using Zod

---
+++ server/schemas/projectSchema.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod';
+
+export const ProjectSchema = z.object({
+  id: z.number(),
+  title: z.string(),
+  desc: z.string(),
+  duration: z.string(),
+  outcomes: z.string(),
+});
--- server/types/Project.ts
+++ server/types/Project.ts
@@ -1,7 +1,16 @@
-export type Project = {
-  id: number;
-  title: string;
-  desc: string;
-  duration: string;
-  outcomes: string;
-};
+// export type Project = {
+//   id: number;
+//   title: string;
+//   desc: string;
+//   duration: string;
+//   outcomes: string;
+// };
+
+import { z } from 'zod';
+
+import { ProjectSchema } from '../schemas/projectSchema';
+
+export type ProjectDto = z.infer<typeof ProjectSchema>;
+export type Project = ProjectDto & { id: number };
+export type CreateProjectInput = Omit<ProjectDto, 'id'>;
+export type UpdateProject = Partial<ProjectDto>;
