d3acaa0cafccdfeadaeec789e18f6d54ce8eca69
Mohamed EL Fechtali <fechtali@Mohameds-MacBook-Pro.local>
2023-11-23 18:08:38 +0200

    education schema

--- server/types/Education.ts
+++ server/types/Education.ts
@@ -1,8 +1,18 @@
-export type Education = {
+  /*export type Education = {
     id: number,
     institute: string;
     degree: string;
     graduation_date: string;
     gpa: string;
     field_of_study: string;
-  };
+  };*/
+
+
+
+import { z } from "zod";
+import { EducationBodySchema } from "../schemas/educationSchema";
+
+export type EducationDto = z.infer<typeof EducationBodySchema>;
+export type Education = EducationDto & { id: number };
+export type CreateEducationInput = EducationDto;
+export type UpdateEducation = Partial<EducationDto>;
